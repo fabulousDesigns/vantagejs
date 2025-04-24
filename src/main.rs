1a8b4e754579f6a6bbabe398e35865b88329afbb
Bernard Maina <hazardmburu10@gmail.com>
2025-04-24 10:28:25 +0300

    Initial commit: VantageJS CLI MVP bootstrapped

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let banner = r#"
+__     ___     _        _              _
+\ \   / (_)___| |_ __ _| |__   ___  __| | ___
+ \ \ / /| / __| __/ _` | '_ \ / _ \/ _` |/ _ \
+  \ V / | \__ \ || (_| | | | |  __/ (_| | (_) |
+   \_/  |_|___/\__\__,_|_| |_|\___|\__,_|\___/
+
+                vantagejs v0.0.1
+    "#;
+
+    println!("{}", banner);
+    println!("\n🚀 CLI initialized! Welcome to the future of JavaScript optimization.\n");
+}
